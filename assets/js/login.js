359ab73fb2ddfce6bb90d6435309ffabd6dba173
Stefanie Sacon <tefisacon@gmail.com>
2024-08-19 22:36:47 -0300

    feat: form-register

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,21 +1,30 @@
-const formularioLogin = document.getElementById("loginForm");
-const formularioCadastro = document.getElementById("cadastroForm");
-const abrirCadastro = document.getElementById("abrirCadastro");
-const abrirLogin = document.getElementById("abrirLogin");
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".show-register").forEach(function (element) {
+    element.addEventListener("click", function (event) {
+      event.preventDefault();
+      showForm("register-form");
+    });
+  });
 
-// Função para alternar entre formulários
-function alternarFormulario() {
-  if (formularioLogin.style.display === "block") {
-    formularioLogin.style.display = "none";
-    formularioCadastro.style.display = "block";
-  } else {
-    formularioLogin.style.display = "block";
-    formularioCadastro.style.display = "none";
+  document.querySelectorAll(".show-login").forEach(function (element) {
+    element.addEventListener("click", function (event) {
+      event.preventDefault();
+      showForm("login-form");
+    });
+  });
+
+  document.querySelectorAll(".forgot-password").forEach(function (element) {
+    element.addEventListener("click", function (event) {
+      event.preventDefault();
+      showForm("password-reset-form");
+    });
+  });
+
+  function showForm(formId) {
+    const forms = document.querySelectorAll(".form-section, .login-form");
+    forms.forEach(function (form) {
+      form.style.display = "none";
+    });
+    document.getElementById(formId).style.display = "block";
   }
-}
-
-// Adicionar eventos de clique aos botões de alternância
-abrirCadastro.addEventListener("click", alternarFormulario);
-abrirLogin.addEventListener("click", alternarFormulario);
-
-// (Adicione mais funções JavaScript aqui para validação, mensagens, etc.)
+});
