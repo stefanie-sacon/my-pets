b0f5bfbe9c1d5c36a028623600d5452a71408efb
Stefanie Sacon <tefisacon@gmail.com>
2024-08-22 09:38:38 -0300

    fix: path trailing space

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react";
-import { Header } from "../components /Header";
-import { Footer } from "../components /Footer";
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 
 export const Index = () => {
   return (
